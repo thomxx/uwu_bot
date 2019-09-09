f3e4ec681f4d95d6d5a5286fa6b40c0b34703d5c
thomxx <55082601+thomxx@users.noreply.github.com>
2019-09-09 17:58:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-let px = 'uwu ';
-let guildname = message.guild.name;
-
 client.once('ready', () => {
 	console.log('Ready!')	
 })
 
 client.on('message', message => {
-	if (message.content === px'hi') {
+	if (message.content === 'uwu hi') {
 	message.channel.send('Hello! uwu');
-} else if (message.content === px'hello') {
+} else if (message.content === 'uwu hello') {
 	message.channel.send('Hi! uwu');
 } else if (message.content === 'uwu server') {
-	message.channel.send('You are currently on 'guildname )
+	message.channel.send('You are currently on' )
 }
 });
 	
