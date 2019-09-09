e800b5f8ded9651f3b2c7a472998a33ad8804858
thomxx <55082601+thomxx@users.noreply.github.com>
2019-09-09 18:31:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 const Discord = require('discord.js');
-const config = require('./config.json');
+const { prefix, token } = require('./config.json');
 const client = new Discord.Client();
 
 client.once('ready', () => {
-	console.log('Ready!')	
-})
-
-//very messy code but it's okay as long as it works! :>
-client.on('message', message => {
-	if (message.content === '${prefix} hi')) {
-	message.channel.send('Hello! uwu');
-} else if (message.content === `${prefix}ping`) {
-	message.channel.send('Pong.');
-} else if (message.content === `${prefix}beep`) {
-	message.channel.send('Boop.');
-} else if (message.content === 'uwu hello') {
-	message.channel.send('Hi! uwu');
-} else if (message.content === 'uwu uwu') {
-	message.channel.send('uwu')
-} 
+	console.log('Ready!');
 });
-	
 
+client.on('message', message => {
+	if (message.content === `${prefix}ping`) {
+		message.channel.send('Pong.');
+	} else if (message.content === `${prefix}beep`) {
+		message.channel.send('Boop.');
+	} else if (message.content === `${prefix}server`) {
+		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+	} else if (message.content === `${prefix}user-info`) {
+		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+	}
+});
 
 client.login(process.env.BOT_TOKEN);
