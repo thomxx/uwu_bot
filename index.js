796acd0df29695163f5c1322bbc190e87823011c
thomxx <55082601+thomxx@users.noreply.github.com>
2019-09-09 18:33:27 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const Discord = require('discord.js');
-const { prefix, token } = require('./config.json');
 const client = new Discord.Client();
 
 client.once('ready', () => {
-	console.log('Ready!');
-});
+	console.log('Ready!')	
+})
 
+//very messy code but it's okay as long as it works! :>
 client.on('message', message => {
-	if (message.content === `${prefix}ping`) {
-		message.channel.send('Pong.');
-	} else if (message.content === `${prefix}beep`) {
-		message.channel.send('Boop.');
-	} else if (message.content === `${prefix}server`) {
-		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-	} else if (message.content === `${prefix}user-info`) {
-		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
-	}
+	if (message.content === 'uwu hi')) {
+	message.channel.send('Hello! uwu');
+} else if (message.content === 'uwu hello') {
+	message.channel.send('Hi! uwu');
+} else if (message.content === 'uwu uwu') {
+	message.channel.send('uwu')
+} 
 });
+	
+
 
 client.login(process.env.BOT_TOKEN);
