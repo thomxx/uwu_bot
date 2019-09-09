5655c841759d017fd2ec6bacedb3df594b9e9c2f
thomxx <55082601+thomxx@users.noreply.github.com>
2019-09-09 16:40:00 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!')	
+})
+
+client.on('message', message => {
+	if (message.content === 'uwu hi') {
+	message.channel.send('Hello! uwu');
+}
+});
+	
+
+
+client.login(token);
